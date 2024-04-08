ec0c36571055954ea27848b5858561e3726b9843
Suman Rai <officeproj111@gmail.com>
2024-04-08 08:29:21 +0545

    Swagger Configured

--- HamroCommunity/HamroCommunity/Program.cs
+++ HamroCommunity/HamroCommunity/Program.cs
@@ -1,8 +1,10 @@
 
 
+using Microsoft.OpenApi.Models;
 using Project.BLL;
 using Project.DLL;
 using Serilog;
+using Swashbuckle.AspNetCore.SwaggerUI;
 
 try
 {
@@ -17,15 +19,96 @@ try
 
     builder.Services.AddControllers();
 
+
+    builder.Services.AddEndpointsApiExplorer();
+
+    #region ConfigureSwaggerForAuthentication
+    builder.Services.AddSwaggerGen(
+option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "HAMROCOMMUNITY API", Version = "V1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type=ReferenceType.SecurityScheme,
+                                    Id="Bearer"
+                                }
+                            },
+                            Array.Empty<string>()
+                        }
+    });
+}
+);
+    #endregion
+
+
+
+
+
+
+
+
+
+
     var app = builder.Build();
 
     // Configure the HTTP request pipeline.
+
+
+  
 
     app.UseHttpsRedirection();
 
     app.UseAuthorization();
 
     app.MapControllers();
+
+  
+
+
+    #region RedirectSwagger
+    //Redirect request from the root Url to swagger UI
+    app.Use(async (context, next) =>
+    {
+        if (context.Request.Path == "/")
+        {
+            context.Response.Redirect("/swagger/index.html");
+            return;
+        }
+
+        await next();
+
+    });
+    #endregion
+
+    #region HttpRequestPipeLine For Swagger
+    if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple Api");
+            c.DocExpansion(DocExpansion.None);
+
+        });
+
+    }
+
+    #endregion
+
 
     app.Run();
 }
