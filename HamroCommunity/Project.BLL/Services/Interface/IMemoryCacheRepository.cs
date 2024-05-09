e2d2ccdd852e871006870e1dc41eb4768b5bff82
Suman Rai <officeproj111@gmail.com>
2024-05-09 22:58:14 +0545

    memory cache added

---
+++ HamroCommunity/Project.BLL/Services/Interface/IMemoryCacheRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Services.Interface
+{
+    public interface IMemoryCacheRepository
+    {
+        Task<T?> GetCacheKey<T>(string cacheKey);
+        Task SetAsync<T>(string cacheKey, T value, MemoryCacheEntryOptions options, CancellationToken cancellationToken);
+        Task RemoveAsync(string cacheKey);
+    }
+}
