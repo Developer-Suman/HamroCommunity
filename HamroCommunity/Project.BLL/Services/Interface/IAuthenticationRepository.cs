2495fe365365400684e0f45c4a27483d864d4b8b
Suman Rai <officeproj111@gmail.com>
2024-05-09 08:20:43 +0545

    Authenticatio Interface works well

---
+++ HamroCommunity/Project.BLL/Services/Interface/IAuthenticationRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Project.BLL.DTOs;
+using Project.DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.Services.Interface
+{
+    public interface IAuthenticationRepository
+    {
+        Task<ApplicationUsers> FindByNameAsync(string userName);
+        Task<ApplicationUsers> FindByIdAsync(string id);
+        Task<ApplicationUsers> FindByEmailAsync(string email);
+        Task<IList<string>> GetRolesAsync(ApplicationUsers username);
+        Task<bool> CheckPasswordAsync(ApplicationUsers username, string password);
+        Task<IdentityResult> CreateUserAsync(ApplicationUsers user, string password);
+        Task UpdateUserAsync(ApplicationUsers user);
+        Task<IdentityResult> CreateRoles(string roles);
+        Task<IdentityResult> AssignRoles(ApplicationUsers user, string rolename);
+        Task<bool> CheckRolesAsync(string role);
+        Task<List<UserDTOs>> GetAllUsersAsync(int page, int pageSize, CancellationToken cancellationToken);
+        Task<UserDTOs> GetById(string  id, CancellationToken cancellationToken);
+    }
+}
