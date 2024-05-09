1bf6703a58e34b86ace4eb2fe14fe641693b8c66
Suman Rai <officeproj111@gmail.com>
2024-05-09 07:44:56 +0545

    JWT Configuration

---
+++ HamroCommunity/Project.DLL/Abstraction/IJwtProviders.cs
@@ -0,0 +1,17 @@
+﻿using Project.DLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DLL.Abstraction
+{
+    public interface IJwtProviders
+    {
+        string Generate(ApplicationUsers users, IList<string> roles);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
