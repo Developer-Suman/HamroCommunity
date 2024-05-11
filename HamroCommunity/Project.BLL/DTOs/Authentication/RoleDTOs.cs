ee2fa3427d1139c5981a37d5f91e37db643de966
Suman Rai <officeproj111@gmail.com>
2024-05-11 23:16:02 +0545

    Works on GetAllRoles except superadmin

---
+++ HamroCommunity/Project.BLL/DTOs/Authentication/RoleDTOs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DTOs.Authentication
+{
+    public class RoleDTOs
+    {
+        public string? RoleId { get; set; }
+        public string? RoleName { get; set;}
+    }
+}
