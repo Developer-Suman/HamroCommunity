e385f2f4d022215a8ef726fd92b1efb5671d538e
Suman Rai <officeproj111@gmail.com>
2024-05-29 02:27:42 +0545

    Add Some RateLimiter

--- HamroCommunity/HamroCommunity/Program.cs
+++ HamroCommunity/HamroCommunity/Program.cs
@@ -1,6 +1,7 @@
 
 using HamroCommunity.Configs;
 using HamroCommunity.CustomMiddleware.GlobalErrorHandling;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.OpenApi.Models;
 using Project.BLL;
 using Project.DLL;
@@ -26,6 +27,58 @@ ConfigurationManager configuration = builder.Configuration;
 builder.Services
     .AddDAL(configuration)
     .AddBLL();
+
+//Configure RateLimiter
+builder.Services.AddRateLimiter(config =>
+{
+    config.AddFixedWindowLimiter("FixedWindowPolicy", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 3;
+        options.QueueLimit = 2;
+        options.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+
+    }).RejectionStatusCode = 429;
+});
+
+
+builder.Services.AddRateLimiter(config =>
+{
+    config.AddSlidingWindowLimiter("SlidingWindowPolicy", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(15);
+        options.PermitLimit = 3;
+        options.QueueLimit = 2;
+        options.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        options.SegmentsPerWindow = 3;
+    }).RejectionStatusCode=  429;
+
+});
+
+builder.Services.AddRateLimiter(config =>
+{
+    config.AddTokenBucketLimiter("TokenBucketPolicy", options =>
+    {
+        options.ReplenishmentPeriod = TimeSpan.FromSeconds(10);
+        options.TokenLimit = 3;
+        options.QueueLimit= 2;
+        options.TokensPerPeriod = 2;
+        options.AutoReplenishment = true;
+        options.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+    }).RejectionStatusCode = 429;
+});
+
+
+builder.Services.AddRateLimiter(config =>
+{
+    config.AddConcurrencyLimiter("ConcurrencyPolicy", options =>
+    {
+        options.PermitLimit = 3;
+        options.QueueLimit = 2;
+        options.QueueProcessingOrder = System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+
+    }).RejectionStatusCode = 429;
+});
 
 // Add services to the container.
 Dependencies.Inject(builder);
