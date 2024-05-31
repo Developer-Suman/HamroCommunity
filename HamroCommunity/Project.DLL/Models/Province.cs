476f172222dcf428a614a854aad82ada92afaa22
Suman Rai <officeproj111@gmail.com>
2024-05-31 18:39:47 +0545

    Add three table using abstract class

---
+++ HamroCommunity/Project.DLL/Models/Province.cs
@@ -0,0 +1,21 @@
+﻿using Project.DLL.Premetives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.DLL.Models
+{
+    public sealed class Province : CustomEntity
+    {
+        public Province(int Id, string provinceNameInNepali, string provinceNameInEnglish) : base(Id)
+        {
+            ProvinceNameInEnglish = provinceNameInEnglish;
+            ProvinceNameInEnglish = provinceNameInEnglish;
+        }
+
+        public string ProvinceNameInNepali { get; set; }
+        public string ProvinceNameInEnglish { get; set; }
+    }
+}
